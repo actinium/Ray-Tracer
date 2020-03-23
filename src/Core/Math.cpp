66cf65e18dcb3de7b07878c82f183c7b2774299f
Andreas Cederholm <andreas.cederholm@gmail.com>
2020-03-23 17:48:05 +0100

    Add Math operations.

--- src/Core/Math.cpp
+++ src/Core/Math.cpp
@@ -1,1 +1,54 @@
+#include "Math.hpp"
 
+namespace {
+void vec4_add(const double (&a)[4], const double (&b)[4], double (&r)[4]) {
+  r[0] = a[0] + b[0];
+  r[1] = a[1] + b[1];
+  r[2] = a[2] + b[2];
+  r[3] = a[3] + b[3];
+}
+
+void vec4_sub(const double (&a)[4], const double (&b)[4], double (&r)[4]) {
+  r[0] = a[0] + b[0];
+  r[1] = a[1] + b[1];
+  r[2] = a[2] + b[2];
+  r[3] = a[3] + b[3];
+}
+
+}  // namespace
+
+//------------------------------------------------------------------------------
+// Addition
+//------------------------------------------------------------------------------
+Vector operator+(const Vector& v1, const Vector& v2) {
+  Vector r;
+  vec4_add(v1.vec4, v2.vec4, r.vec4);
+  return r;
+}
+
+Point operator+(const Point& p, const Vector& v) {
+  Point r;
+  vec4_add(p.vec4, v.vec4, r.vec4);
+  return r;
+}
+
+//------------------------------------------------------------------------------
+// Subtraction
+//------------------------------------------------------------------------------
+Vector operator-(const Vector& v1, const Vector& v2) {
+  Vector r;
+  vec4_sub(v1.vec4, v2.vec4, r.vec4);
+  return r;
+}
+
+Vector operator-(const Point& p1, const Point& p2) {
+  Vector r;
+  vec4_sub(p1.vec4, p2.vec4, r.vec4);
+  return r;
+}
+
+Point operator-(const Point& p, const Vector& v) {
+  Point r;
+  vec4_sub(p.vec4, v.vec4, r.vec4);
+  return r;
+}
--- src/Core/Math.hpp
+++ src/Core/Math.hpp
@@ -3,3 +3,66 @@
 #include "Matrix.hpp"
 #include "Point.hpp"
 #include "Vector.hpp"
+
+//------------------------------------------------------------------------------
+// Addition
+//------------------------------------------------------------------------------
+Vector operator+(const Vector& v1, const Vector& v2);
+Point operator+(const Point& p, const Vector& v);
+
+//------------------------------------------------------------------------------
+// Subtraction
+//------------------------------------------------------------------------------
+Vector operator-(const Vector& v1, const Vector& v2);
+Vector operator-(const Point& p1, const Point& p2);
+Point operator-(const Point& p, const Vector& v);
+
+//------------------------------------------------------------------------------
+// Negation
+//------------------------------------------------------------------------------
+Vector operator-(const Vector& v);
+Point operator-(const Point& p);
+
+//------------------------------------------------------------------------------
+// Multiplication
+//------------------------------------------------------------------------------
+Matrix operator*(const Matrix& m1, const Matrix& m2);
+Vector operator*(const Matrix& m, const Vector& v);
+Point operator*(const Matrix& m, const Point& p);
+
+Vector cross(const Vector& v1, const Vector& v2);
+double dot(const Vector& v1, const Vector& v2);
+
+Vector operator*(double s, const Vector& v);
+Vector operator*(const Vector& v, double s);
+Point operator*(double s, const Point& p);
+Point operator*(const Point& p, double s);
+
+//------------------------------------------------------------------------------
+// Division
+//------------------------------------------------------------------------------
+Vector operator/(double s, const Vector& v);
+Vector operator/(const Vector& v, double s);
+Point operator/(double s, const Point& p);
+Point operator/(const Point& p, double s);
+
+//------------------------------------------------------------------------------
+// Magnitude
+//------------------------------------------------------------------------------
+double magnitude(const Vector& v);
+Vector normalize(const Vector& v);
+
+//------------------------------------------------------------------------------
+// Reflection
+//------------------------------------------------------------------------------
+Vector reflect(const Vector& v, const Vector& n);
+
+//------------------------------------------------------------------------------
+// Transposition
+//------------------------------------------------------------------------------
+Matrix transpose(const Matrix& m);
+
+//------------------------------------------------------------------------------
+// Inversion
+//------------------------------------------------------------------------------
+Matrix inverse(const Matrix& m);
