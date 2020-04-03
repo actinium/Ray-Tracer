ab072e1e19eb9c610438632937e0d1054432cf4e
Andreas Cederholm <andreas.cederholm@gmail.com>
2020-04-03 22:58:46 +0200

    Add "wrapper header" for 'stb_image_write.h'.

---
+++ vendor/nothings/stb/image_writer.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wconversion"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#pragma clang diagnostic pop
