37e9c7ad4893d580ee775c60ca355c832c3cbd1f
Andreas Cederholm <andreas.cederholm@gmail.com>
2020-07-26 23:32:22 +0200

    Add matrix construction.

--- src/Core/Matrix.hpp
+++ src/Core/Matrix.hpp
@@ -5,9 +5,38 @@
 class Matrix {
  public:
   constexpr Matrix() noexcept;
-  constexpr Matrix(
-      std::initializer_list<std::initializer_list<double>> im) noexcept;
+  constexpr Matrix(double (&m)[4][4]) noexcept;
+  constexpr Matrix& operator=(const Matrix& other) noexcept;
 
  public:
   alignas(32) double mat4[4][4];
 };
+
+constexpr Matrix::Matrix() noexcept
+    : mat4{{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}} {}
+
+constexpr Matrix::Matrix(double (&m)[4][4]) noexcept
+    : mat4{{m[0][0], m[0][1], m[0][2], m[0][3]},
+           {m[1][0], m[1][1], m[1][2], m[1][3]},
+           {m[2][0], m[2][1], m[2][2], m[2][3]},
+           {m[3][0], m[3][1], m[3][2], m[3][3]}} {}
+
+constexpr Matrix& Matrix::operator=(const Matrix& other) noexcept {
+  mat4[0][0] = other.mat4[0][0];
+  mat4[0][1] = other.mat4[0][1];
+  mat4[0][2] = other.mat4[0][2];
+  mat4[0][3] = other.mat4[0][3];
+  mat4[1][0] = other.mat4[1][0];
+  mat4[1][1] = other.mat4[1][1];
+  mat4[1][2] = other.mat4[1][2];
+  mat4[1][3] = other.mat4[1][3];
+  mat4[2][0] = other.mat4[2][0];
+  mat4[2][1] = other.mat4[2][1];
+  mat4[2][2] = other.mat4[2][2];
+  mat4[2][3] = other.mat4[2][3];
+  mat4[3][0] = other.mat4[3][0];
+  mat4[3][1] = other.mat4[3][1];
+  mat4[3][2] = other.mat4[3][2];
+  mat4[3][3] = other.mat4[3][3];
+  return *this;
+}
--- tests/Matrix.test.cpp
+++ tests/Matrix.test.cpp
@@ -3,3 +3,47 @@
 #include "Core/Math.hpp"
 #include "Core/Matrix.hpp"
 #include "Core/Vector.hpp"
+
+TEST_CASE("The default matrix is the identity matrix", "[Matrix]") {
+  Matrix m;
+  REQUIRE(m.mat4[0][0] == Approx(1));
+  REQUIRE(m.mat4[0][1] == Approx(0));
+  REQUIRE(m.mat4[0][2] == Approx(0));
+  REQUIRE(m.mat4[0][3] == Approx(0));
+  REQUIRE(m.mat4[1][0] == Approx(0));
+  REQUIRE(m.mat4[1][1] == Approx(1));
+  REQUIRE(m.mat4[1][2] == Approx(0));
+  REQUIRE(m.mat4[1][3] == Approx(0));
+  REQUIRE(m.mat4[2][0] == Approx(0));
+  REQUIRE(m.mat4[2][1] == Approx(0));
+  REQUIRE(m.mat4[2][2] == Approx(1));
+  REQUIRE(m.mat4[2][3] == Approx(0));
+  REQUIRE(m.mat4[3][0] == Approx(0));
+  REQUIRE(m.mat4[3][1] == Approx(0));
+  REQUIRE(m.mat4[3][2] == Approx(0));
+  REQUIRE(m.mat4[3][3] == Approx(1));
+}
+
+TEST_CASE("Matrix construction", "[Matrix]") {
+  double mat4[4][4] = {{1, 2, 3, 4},
+                       {-1, -2, -3, -4},
+                       {0.1, 0.02, 0.003, 0.0004},
+                       {555, 0, 1.0, 4.3333}};
+  Matrix m(mat4);
+  REQUIRE(m.mat4[0][0] == Approx(1));
+  REQUIRE(m.mat4[0][1] == Approx(2));
+  REQUIRE(m.mat4[0][2] == Approx(3));
+  REQUIRE(m.mat4[0][3] == Approx(4));
+  REQUIRE(m.mat4[1][0] == Approx(-1));
+  REQUIRE(m.mat4[1][1] == Approx(-2));
+  REQUIRE(m.mat4[1][2] == Approx(-3));
+  REQUIRE(m.mat4[1][3] == Approx(-4));
+  REQUIRE(m.mat4[2][0] == Approx(0.1));
+  REQUIRE(m.mat4[2][1] == Approx(0.02));
+  REQUIRE(m.mat4[2][2] == Approx(0.003));
+  REQUIRE(m.mat4[2][3] == Approx(0.0004));
+  REQUIRE(m.mat4[3][0] == Approx(555));
+  REQUIRE(m.mat4[3][1] == Approx(0));
+  REQUIRE(m.mat4[3][2] == Approx(1));
+  REQUIRE(m.mat4[3][3] == Approx(4.3333));
+}
