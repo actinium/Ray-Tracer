503ba3ce80035d297a3e20141e7754f1da9e5ebd
Andreas Cederholm <andreas.cederholm@gmail.com>
2020-07-29 22:47:18 +0200

    Add matrix test matcher.

--- tests/Matrix.test.cpp
+++ tests/Matrix.test.cpp
@@ -1,27 +1,13 @@
-#include "catch.hpp"
-
 #include "Core/Math.hpp"
 #include "Core/Matrix.hpp"
 #include "Core/Vector.hpp"
+#include "TestUtils.hpp"
+#include "catch.hpp"
 
 TEST_CASE("The default matrix is the identity matrix", "[Matrix]") {
   Matrix m;
-  REQUIRE(m.mat4[0][0] == Approx(1));
-  REQUIRE(m.mat4[0][1] == Approx(0));
-  REQUIRE(m.mat4[0][2] == Approx(0));
-  REQUIRE(m.mat4[0][3] == Approx(0));
-  REQUIRE(m.mat4[1][0] == Approx(0));
-  REQUIRE(m.mat4[1][1] == Approx(1));
-  REQUIRE(m.mat4[1][2] == Approx(0));
-  REQUIRE(m.mat4[1][3] == Approx(0));
-  REQUIRE(m.mat4[2][0] == Approx(0));
-  REQUIRE(m.mat4[2][1] == Approx(0));
-  REQUIRE(m.mat4[2][2] == Approx(1));
-  REQUIRE(m.mat4[2][3] == Approx(0));
-  REQUIRE(m.mat4[3][0] == Approx(0));
-  REQUIRE(m.mat4[3][1] == Approx(0));
-  REQUIRE(m.mat4[3][2] == Approx(0));
-  REQUIRE(m.mat4[3][3] == Approx(1));
+  REQUIRE_THAT(m,
+               Equals(Matrix(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)));
 }
 
 TEST_CASE("Matrix construction", "[Matrix]") {
@@ -30,20 +16,6 @@ TEST_CASE("Matrix construction", "[Matrix]") {
                        {0.1, 0.02, 0.003, 0.0004},
                        {555, 0, 1.0, 4.3333}};
   Matrix m(mat4);
-  REQUIRE(m.mat4[0][0] == Approx(1));
-  REQUIRE(m.mat4[0][1] == Approx(2));
-  REQUIRE(m.mat4[0][2] == Approx(3));
-  REQUIRE(m.mat4[0][3] == Approx(4));
-  REQUIRE(m.mat4[1][0] == Approx(-1));
-  REQUIRE(m.mat4[1][1] == Approx(-2));
-  REQUIRE(m.mat4[1][2] == Approx(-3));
-  REQUIRE(m.mat4[1][3] == Approx(-4));
-  REQUIRE(m.mat4[2][0] == Approx(0.1));
-  REQUIRE(m.mat4[2][1] == Approx(0.02));
-  REQUIRE(m.mat4[2][2] == Approx(0.003));
-  REQUIRE(m.mat4[2][3] == Approx(0.0004));
-  REQUIRE(m.mat4[3][0] == Approx(555));
-  REQUIRE(m.mat4[3][1] == Approx(0));
-  REQUIRE(m.mat4[3][2] == Approx(1));
-  REQUIRE(m.mat4[3][3] == Approx(4.3333));
+  REQUIRE_THAT(m, Equals(Matrix(1, 2, 3, 4, -1, -2, -3, -4, 0.1, 0.02, 0.003,
+                                0.0004, 555, 0, 1, 4.3333)));
 }
--- tests/TestUtils.hpp
+++ tests/TestUtils.hpp
@@ -2,10 +2,12 @@
 
 #include <sstream>
 
+#include "Core/Matrix.hpp"
+#include "Core/Point.hpp"
 #include "Core/Vector.hpp"
 #include "catch.hpp"
 
-template<class T>
+template <class T>
 class TupleEquals : public Catch::MatcherBase<T> {
   T expected;
 
@@ -39,7 +41,56 @@ inline TupleEquals<Vector> Equals(const Vector& expected) {
 //------------------------------------------------------------------------------
 // Point
 //------------------------------------------------------------------------------
+inline std::ostream& operator<<(std::ostream& os, const Point& p) {
+  os << "(" << p.x << "," << p.y << "," << p.z << "," << p.w << ")";
+  return os;
+}
+
+inline TupleEquals<Point> Equals(const Point& expected) {
+  return TupleEquals<Point>(expected);
+}
 
 //------------------------------------------------------------------------------
 // Matrix
 //------------------------------------------------------------------------------
+inline std::ostream& operator<<(std::ostream& os, const Matrix& m) {
+  os << "[";
+  for (int r = 0; r < 4; ++r) {
+    os << "[";
+    for (int c = 0; c < 4; ++c) {
+      os << m.mat4[r][c];
+      os << (c < 3 ? "," : "");
+    }
+    os << "]" << (r < 3 ? "," : "");
+  }
+  os << "]";
+  return os;
+}
+
+class MatrixEquals : public Catch::MatcherBase<Matrix> {
+  Matrix expected;
+
+ public:
+  MatrixEquals(const Matrix& e) : expected{e} {}
+
+  bool match(const Matrix& m) const override {
+    for (int r = 0; r < 4; ++r) {
+      for (int c = 0; c < 4; ++c) {
+        if (expected.mat4[r][c] != Approx(m.mat4[r][c])) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  virtual std::string describe() const override {
+    std::ostringstream ss;
+    ss << "equals " << expected;
+    return ss.str();
+  }
+};
+
+inline MatrixEquals Equals(const Matrix& expected) {
+  return MatrixEquals(expected);
+}
