7dc749504571c1db83b21af275b7962a36420d72
Andreas Cederholm <andreas.cederholm@gmail.com>
2020-08-03 23:40:12 +0200

    Add negation to vector & point.

--- src/Core/Math.cpp
+++ src/Core/Math.cpp
@@ -9,10 +9,17 @@ void vec4_add(const double (&a)[4], const double (&b)[4], double (&r)[4]) {
 }
 
 void vec4_sub(const double (&a)[4], const double (&b)[4], double (&r)[4]) {
-  r[0] = a[0] + b[0];
-  r[1] = a[1] + b[1];
-  r[2] = a[2] + b[2];
-  r[3] = a[3] + b[3];
+  r[0] = a[0] - b[0];
+  r[1] = a[1] - b[1];
+  r[2] = a[2] - b[2];
+  r[3] = a[3] - b[3];
+}
+
+void vec4_neg(const double (&a)[4], double (&r)[4]) {
+  r[0] = -a[0];
+  r[1] = -a[1];
+  r[2] = -a[2];
+  r[3] = -a[3];
 }
 
 }  // namespace
@@ -50,5 +57,19 @@ Vector operator-(const Point& p1, const Point& p2) {
 Point operator-(const Point& p, const Vector& v) {
   Point r;
   vec4_sub(p.vec4, v.vec4, r.vec4);
+  return r;
+}
+
+//------------------------------------------------------------------------------
+// Negation
+//------------------------------------------------------------------------------
+Vector operator-(const Vector& v) {
+  Vector r;
+  vec4_neg(v.vec4, r.vec4);
+  return r;
+}
+Point operator-(const Point& p) {
+  Point r;
+  vec4_neg(p.vec4, r.vec4);
   return r;
 }
--- tests/Vector.test.cpp
+++ tests/Vector.test.cpp
@@ -1,4 +1,5 @@
 #include "Core/Math.hpp"
+#include "Core/Point.hpp"
 #include "Core/Vector.hpp"
 #include "TestUtils.hpp"
 #include "catch.hpp"
@@ -8,9 +9,37 @@ TEST_CASE("The default vector is (0, 0, 0, 0)", "[Vector]") {
   REQUIRE_THAT(v, Equals(Vector(0, 0, 0)));
 }
 
+//------------------------------------------------------------------------------
+// Addition
+//------------------------------------------------------------------------------
 TEST_CASE("Add two vectors", "[Vector]") {
   Vector v1(1.1, 2.3, 3.0);
   Vector v2(8.5, -1, 0);
   Vector v = v1 + v2;
   REQUIRE_THAT(v, Equals(Vector(9.6, 1.3, 3.0)));
+}
+
+//------------------------------------------------------------------------------
+// Subtraction
+//------------------------------------------------------------------------------
+TEST_CASE("Subtracting a vector from a vector", "[Vector]") {
+  Vector v1(1.1, 2.3, 3.0);
+  Vector v2(8.5, -1, 0);
+  Vector v = v1 - v2;
+  REQUIRE_THAT(v, Equals(Vector(-7.4, 3.3, 3.0)));
+}
+
+TEST_CASE("Subtracting a point from a point", "[Vector]") {
+  Point p1(1.1, -2.3, 4.0);
+  Point p2(4.1, 1, 0);
+  Vector v = p1 - p2;
+  REQUIRE_THAT(v, Equals(Vector(-3, -3.3, 4.0)));
+}
+
+//------------------------------------------------------------------------------
+// Negation
+//------------------------------------------------------------------------------
+TEST_CASE("Negating a vector", "[Vector]") {
+  Vector v(1.1, -2.3, 3.1234);
+  REQUIRE_THAT(-v, Equals(Vector(-1.1, 2.3, -3.1234)));
 }
