efb72df42171810e856fbcd4428f78ed90d1e1c3
Andreas Cederholm <andreas.cederholm@gmail.com>
2020-10-30 23:34:12 +0100

    Implement Triangle.

--- src/Scene/Objects/Triangle.cpp
+++ src/Scene/Objects/Triangle.cpp
@@ -1,1 +1,33 @@
 #include "Triangle.hpp"
+
+#include <cmath>
+
+#include "Core/Constants.hpp"
+
+using std::abs;
+
+Intersections Triangle::local_intersect(const Ray& ray) const {
+  Vector dir_cross_e2 = cross(ray.direction, e2);
+  double det = dot(e1, dir_cross_e2);
+  if (abs(det) < EPSILON) {
+    return {};
+  }
+
+  double f = 1.0 / det;
+  Vector p1_to_origin = ray.origin - p1;
+  double u = f * dot(p1_to_origin, dir_cross_e2);
+  if (u < 0 || u > 1) {
+    return {};
+  }
+
+  Vector origin_cross_e1 = cross(p1_to_origin, e1);
+  double v = f * dot(ray.direction, origin_cross_e1);
+  if (v < 0 || (u + v) > 1) {
+    return {};
+  }
+
+  double t = f * dot(e2, origin_cross_e1);
+  return {Intersection(t, this)};
+}
+
+Vector Triangle::local_normal_at(const Point&) const { return normal; }
--- src/Scene/Objects/Triangle.hpp
+++ src/Scene/Objects/Triangle.hpp
@@ -1,5 +1,33 @@
 #pragma once
 
+#include "Core/Point.hpp"
 #include "Scene/Objects/Object.hpp"
 
-class Trinagle {};
+class Triangle : public Object {
+ public:
+  Triangle(const Point& p1, const Point& p2, const Point& p3) noexcept;
+
+ private:
+  Intersections local_intersect(const Ray& ray) const override;
+  Vector local_normal_at(const Point& point) const override;
+
+ public:
+  Point p1;
+  Point p2;
+  Point p3;
+
+  Vector e1;
+  Vector e2;
+
+  Vector normal;
+};
+
+inline Triangle::Triangle(const Point& p1_, const Point& p2_,
+                          const Point& p3_) noexcept
+    : Object(),
+      p1(p1_),
+      p2(p2_),
+      p3(p3_),
+      e1(p2 - p1),
+      e2(p3 - p1),
+      normal(normalize(cross(e2, e1))) {}
